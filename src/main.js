d307413a58c018a9252b49214894357e73e79dd4
paedayz <pae16phasit@gmail.com>
2020-07-06 01:06:01 +0700

    Fixed Config

--- src/main.js
+++ src/main.js
@@ -5,19 +5,11 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import firebase from "firebase/app";
 import "firebase/firestore";
+import config from "./config";
 
 Vue.config.productionTip = false;
 
-firebase.initializeApp({
-  apiKey: "AIzaSyAwYqgjHbYMj5z0m5plyConh0UJU_-1K8I",
-  authDomain: "ilog-work1.firebaseapp.com",
-  databaseURL: "https://ilog-work1.firebaseio.com",
-  projectId: "ilog-work1",
-  storageBucket: "ilog-work1.appspot.com",
-  messagingSenderId: "565560732638",
-  appId: "1:565560732638:web:82f77b2e7979d0bb8b0b4f",
-  measurementId: "G-LZ290H6Y5W",
-});
+firebase.initializeApp(config);
 
 export const db = firebase.firestore();
 
