467aa314ec795bc634592c227b4ff9dd198d8605
paedayz <pae16phasit@gmail.com>
2020-07-06 12:02:32 +0700

    Created Login Form

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Data_table from "../views/DataTable.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/DataTable",
+    name: "DataTable",
+    component: Data_table,
   },
 ];
 
